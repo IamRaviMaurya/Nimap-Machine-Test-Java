7697a344515924c6499e335033fb3b3518278228
IamRaviMaurya <82931953+IamRaviMaurya@users.noreply.github.com>
2024-10-01 19:07:21 +0530

    worked on Nimap Machine Test API

--- src/main/java/machinetest/nimap/config/OpneApiConfig.java
+++ src/main/java/machinetest/nimap/config/OpneApiConfig.java
@@ -3,16 +3,20 @@ package machinetest.nimap.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpneApiConfig {
 
+    @Value("${server.url}")
+    private String serverUrl;
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .addServersItem(new Server().url("https://nimap-machine-test-java-production.up.railway.app"))
+                .addServersItem(new Server().url(serverUrl))
                 .info(new Info().title("Your API Title")
                         .version("1.0")
                         .description("API description"));
