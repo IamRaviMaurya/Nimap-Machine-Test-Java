45b65bdfa8dbb2d361c5850be49b29b0861c7325
IamRaviMaurya <82931953+IamRaviMaurya@users.noreply.github.com>
2024-10-01 12:13:57 +0530

    feat: Initial commit

---
+++ src/test/java/machinetest/nimap/machine_test/MachineTestApplicationTests.java
@@ -0,0 +1,13 @@
+package machinetest.nimap.machine_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MachineTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
